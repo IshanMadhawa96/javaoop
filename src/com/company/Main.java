e783619d5bdc19b65578e46025cce75dae679ef7
Ishan Madhawa <ishan@ninodigitalhouse.com>
2022-07-01 11:47:54 +0530

    add class an object coding

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,15 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        //create object
+        Car c1 = new Car();
+        //c1 is a instance
+        c1.weight =250; // initialize attribute
+        c1.number =155;
+        c1.color = "red";
+        c1.name = "lambo";
+        c1.speed = 750;
+        c1.speed();//method
+        c1.color();
     }
 }
